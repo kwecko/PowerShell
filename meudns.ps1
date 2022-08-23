ba9dd89ab602f3f92a94b3f00981e2ced578a095
Marcelo Rios Kwecko <59372283+kwecko@users.noreply.github.com>
2022-08-23 12:13:03 -0300

    Add 2
    Adicionado um segundo método

--- meudns.ps1
+++ meudns.ps1
@@ -1,2 +1,7 @@
-#Script que informa qual o IP do servidor de DNS padr„o do sistema
-(Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -eq $(Get-NetIPAddress | Where-Object {$_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00"}).InterfaceAlias }).ServerAddresses
+# Script que informa qual o IP do servidor de DNS padr√£o do sistema
+
+# Metodo 01
+#(Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -eq $(Get-NetIPAddress | Where-Object {$_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00"}).InterfaceAlias }).ServerAddresses
+
+# Metodo 02
+(Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceIndex -eq $(Get-NetIPConfiguration | Foreach IPv4DefaultGateway).ifIndex}).ServerAddresses
