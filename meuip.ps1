c3387dbb4ee210c1f607f676e5f54fa3285413ad
Marcelo Rios Kwecko <59372283+kwecko@users.noreply.github.com>
2022-08-23 12:14:18 -0300

    Add 2
    Adicionado 2 método

--- meuip.ps1
+++ meuip.ps1
@@ -1,2 +1,7 @@
 # Script que informa qual o IPv4 padrão do sistema
+
+# Metodo 01
+# (Get-NetIPAddress | Where-Object {$_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00"}).IPAddress
+
+# Metodo 02
 (Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceIndex -eq $(Get-NetIPConfiguration | Foreach IPv4DefaultGateway).ifIndex}).IPAddress
